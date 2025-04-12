273399b33da40e5b8b735b0a14b1a911cc37b780
jerome-mathew-devops <kidfame47@gmail.com>
2025-04-12 15:52:41 +0100

    Added work

---
+++ Tuples/Access Tuples Items.py
@@ -0,0 +1,26 @@
+tuple1 = ("Rohan", "Physics", 21, 69.75)
+tuple2 = (1, 2, 3, 4, 5)
+
+print ("Item at 0th index in tuple1: ", tuple1[0])
+print ("Item at index 2 in tuple2: ", tuple2[2])
+
+tup1 = ("a", "b", "c", "d")
+tup2 = (1, 2, 3, 4, 5)
+
+print ("Items from index 1 to last in tup1: ", tup1[1:])
+print ("Items from index 2 to last in tup2", tup2[2:-1])
+
+tuple1 = ("a", "b", "c", "d")
+tuple2 = (25.50, True, -55, 1+2j)
+tuple3 = (1, 2, 3, 4, 5)
+tuple4 = ("Rohan", "Physics", 21, 69.75)
+
+print ("Items from index 1 to last in tuple1: ", tuple1[1:])
+print ("Items from index 0 to 1 in tuple2: ", tuple2[:2])
+print ("Items from index 0 to index last in tuple3", tuple3[:])
+
+tuple1 = ("a", "b", "c", "d")
+tuple2 = (25.50, True, -55, 1+2j)
+
+print ("Items from index 1 to 2 in tuple1: ", tuple1[1:3])
+print ("Items from index 0 to 1 in tuple2: ", tuple2[0:2])
---
+++ Tuples/Update Tuples.py
@@ -0,0 +1,7 @@
+# Original tuple
+T1 = (10, 20, 30, 40)
+# Tuple to be concatenated
+T2 = ('one', 'two', 'three', 'four')
+# Updating the tuple using the concatenation operator
+T1 = T1 + T2
+print(T1)
